2157bec85ec625d6abd6845632053b38b2920536
SEC <c3392302@naver.com>
2023-05-03 12:00:12 +0900

    feat: 자판 보유 금액 유효성 검사하기

---
+++ src/main/java/vendingmachine/validate/InputValidation.java
@@ -0,0 +1,22 @@
+package vendingmachine.validate;
+
+public class InputValidation {
+
+    public static void validateMachineHavingMoney(String input){
+        if(isInputEqualsNumber(input)){
+            if(isDivideBy10(input)){
+                return;
+            }
+        }
+        throw new IllegalArgumentException("[ERROR] 10으로 나누어지는 숫자를 입력하세요.");
+    }
+
+    private static boolean isInputEqualsNumber(String input){
+        String reg = "^[0-9]+$";
+        return input.matches(reg);
+    }
+
+    private static boolean isDivideBy10(String input){
+        return Integer.parseInt(input) % 10 == 0;
+    }
+}
