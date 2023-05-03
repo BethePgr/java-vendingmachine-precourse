6a1ba967447dc517a7d225d4062e05c9c4eb21d5
SEC <c3392302@naver.com>
2023-05-03 12:00:01 +0900

    feat: 자판 보유 금액 입력받기

---
+++ src/main/java/vendingmachine/view/InputView.java
@@ -0,0 +1,12 @@
+package vendingmachine.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static String inputMachineHavingMoney(){
+        System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
+        String input = Console.readLine();
+        return input;
+    }
+}
