63326ded27a1c19ae1dfa191a8f0f511dea9e246
SEC <c3392302@naver.com>
2023-05-03 12:49:35 +0900

    feat: Coin의 필드 중 amount 값을 무작위로 가져오는 기능

---
+++ src/main/java/vendingmachine/domain/Coin.java
@@ -0,0 +1,32 @@
+package vendingmachine.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public enum Coin {
+    COIN_500(500),
+    COIN_100(100),
+    COIN_50(50),
+    COIN_10(10);
+
+    private final int amount;
+
+
+    Coin(final int amount) {
+        this.amount = amount;
+    }
+
+    // 추가 기능 구현
+    public static int getRandomCoin(){
+        List<Integer> coins = Arrays.stream(Coin.values()).map(coin -> coin.amount)
+            .collect(Collectors.toList());
+
+        return Randoms.pickNumberInList(coins);
+    }
+
+    public int getAmount(){
+        return amount;
+    }
+}
