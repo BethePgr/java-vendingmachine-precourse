e072350459288386d4d451dd572055acab6e9680
SEC <c3392302@naver.com>
2023-05-03 12:00:28 +0900

    test: 자판 보유 금액 유효성 검사 테스트

---
+++ src/test/java/vendingmachine/validate/InputValidationTest.java
@@ -0,0 +1,44 @@
+package vendingmachine.validate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class InputValidationTest {
+
+    @Test
+    @DisplayName("10으로 나누어지지 않으므로 에러를 발생시킨다.")
+    void notDivideBy10() throws Exception {
+        //given
+        String input = "11";
+        //then
+        assertThrows(IllegalArgumentException.class,() -> InputValidation.validateMachineHavingMoney(input));
+    }
+
+    @Test
+    @DisplayName("숫자로만 이루어져 있지 않으므로 에러를 발생시킨다.")
+    void notOnlyNumber() throws Exception {
+        //given
+        String input = "ab10";
+        //then
+        assertThrows(IllegalArgumentException.class,()->InputValidation.validateMachineHavingMoney(input));
+    }
+
+    @Test
+    @DisplayName("10으로 나누어 떵러지면서 숫자로만 이루어져 있으므로 에러를 발생시키지 않는다.")
+    void divideBy10AndOnlyNumber1() throws Exception {
+        //given
+        String input = "10";
+        //then
+        assertDoesNotThrow(() -> InputValidation.validateMachineHavingMoney(input));
+    }
+    @Test
+    @DisplayName("10으로 나누어 떵러지면서 숫자로만 이루어져 있으므로 에러를 발생시키지 않는다.")
+    void divideBy10AndOnlyNumber2() throws Exception {
+        //given
+        String input = "120";
+        //then
+        assertDoesNotThrow(() -> InputValidation.validateMachineHavingMoney(input));
+    }
+}
