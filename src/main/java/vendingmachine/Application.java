5c2987b543bf362bfe4bdaf9cc760ebbab49cf26
jongwonpc <c3392302@naver.com>
2023-05-04 18:21:48 +0900

    feat: 상품개수가 0개가 아닌 것들 중에서 최소 가격 물품 갱신하는 기능

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,12 +1,15 @@
 package vendingmachine;
 
+import java.util.HashMap;
+import java.util.Map;
 import vendingmachine.controller.VendingMachineController;
+import vendingmachine.domain.Product;
+import vendingmachine.service.VendingMachineService;
 
 public class Application {
 
     public static void main(String[] args) {
         VendingMachineController vendingMachineController = new VendingMachineController();
         vendingMachineController.run();
-
     }
 }
---
+++ src/main/java/vendingmachine/service/VendingMachineService.java
@@ -0,0 +1,23 @@
+package vendingmachine.service;
+
+import java.util.Map;
+import vendingmachine.domain.Product;
+import vendingmachine.domain.VendingMachine;
+
+public class VendingMachineService {
+
+    private final VendingMachine vendingMachine;
+    private int clientMoney;
+
+    public VendingMachineService(VendingMachine vendingMachine, int clientMoney){
+        this.vendingMachine = vendingMachine;
+        this.clientMoney = clientMoney;
+    }
+
+    public int findLeastPrice(Map<Product,Integer> itemMap){
+        return itemMap.entrySet().stream()
+            .filter(item -> item.getValue() != 0)
+            .min((e1, e2) -> e1.getKey().getPrice() - e2.getKey().getPrice()).get().getKey().getPrice();
+    }
+
+}
