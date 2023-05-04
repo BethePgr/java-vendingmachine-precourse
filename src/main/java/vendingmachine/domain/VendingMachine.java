8a38815cc415ec99e04d0f0dcc52ec1123dbceee
jongwonpc <c3392302@naver.com>
2023-05-04 16:58:34 +0900

    feat: vendingMachine 에 상품 등록하는 기능

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -1,37 +1,53 @@
 package vendingmachine.domain;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class VendingMachine {
 
-    private Map<Integer,Integer> coinList;
+    private Map<Integer, Integer> coinMap;
     private final int money;
+    private Map<Product, Integer> itemMap;
 
-    public VendingMachine(int money){
+    public VendingMachine(int money) {
         this.money = money;
         initMap();
         makeCoinListWithRandoms();
     }
-    private void initMap(){
-        coinList = new HashMap<>();
-        for(Coin coin : Coin.values()){
-            coinList.put(coin.getAmount(),0);
+
+    private void initMap() {
+        coinMap = new HashMap<>();
+        for (Coin coin : Coin.values()) {
+            coinMap.put(coin.getAmount(), 0);
         }
     }
 
-    private void makeCoinListWithRandoms(){
+    private void makeCoinListWithRandoms() {
         int tempMoney = money;
-        while(tempMoney != 0) {
+        while (tempMoney != 0) {
             int randomCoin = Coin.getRandomCoin();
-            if(tempMoney >= randomCoin){
-                coinList.put(randomCoin,coinList.get(randomCoin)+1);
+            if (tempMoney >= randomCoin) {
+                coinMap.put(randomCoin, coinMap.get(randomCoin) + 1);
                 tempMoney -= randomCoin;
             }
         }
     }
 
+    private void makeItemMap(List<String> itemList) {
+        for (String item : itemList) {
+            item = item.substring(1, item.length() - 1);
+            String[] splits = item.split(",");
+            itemMap.put(new Product(splits[0], Integer.parseInt(splits[1])),
+                Integer.parseInt(splits[3]));
+        }
+    }
+
     public Map<Integer, Integer> getCoinList() {
-        return coinList;
+        return coinMap;
+    }
+
+    public Map<Product,Integer> getItemMap(){
+        return itemMap;
     }
 }
