e9d69c935edf8b2e8898f5164c40c5312b9a0e34
jongwonpc <c3392302@naver.com>
2023-05-05 10:20:52 +0900

    test: 오류 수정

--- src/test/java/vendingmachine/domain/VendingMachineTest.java
+++ src/test/java/vendingmachine/domain/VendingMachineTest.java
@@ -10,6 +10,7 @@ import java.util.Map.Entry;
 import java.util.Random;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import vendingmachine.util.UtilService;
 
 class VendingMachineTest {
 
@@ -20,11 +21,11 @@ class VendingMachineTest {
         int realMoney = (int)((Math.random()+1)*10000);
         realMoney = realMoney - realMoney%10;
         for(int i = 0;i<5000;i++) {
-            Map<Integer, Integer> coinMap = new HashMap<>();
+            Map<Integer, Integer> coinMap = UtilService.makeCoinListWithRandoms(realMoney);
             List<String> itemList = new ArrayList<>();
             VendingMachine vendingMachine = new VendingMachine(realMoney,coinMap,itemList);
             int expectMoney = 0;
-            for (Entry<Integer, Integer> entry : vendingMachine.getCoinList().entrySet()) {
+            for (Entry<Integer, Integer> entry : vendingMachine.getCoinMap().entrySet()) {
                 expectMoney += entry.getKey() * entry.getValue();
             }
             assertEquals(realMoney, expectMoney);
