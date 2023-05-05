f1c04c694918df3478a06a35b33bd1baf1984b49
jongwonpc <c3392302@naver.com>
2023-05-05 14:37:17 +0900

    refactor: 컨트롤러 리팩토링

--- src/main/java/vendingmachine/controller/VendingMachineController.java
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -11,19 +11,35 @@ import vendingmachine.view.OutputView;
 public class VendingMachineController {
 
     public void run(){
-        int money = InputController.machineHavingMoney();
-        Map<Integer, Integer> coinMap = UtilService.makeCoinListWithRandoms(money);
-        OutputView.printCoinMap(coinMap);
-        List<String> items = InputController.machineHavingItems();
-        VendingMachine vendingMachine = new VendingMachine(money, coinMap, items);
-        VendingMachineService vendingMachineService = new VendingMachineService(vendingMachine,
-            InputController.clientToMachineMoney());
-        while(vendingMachineService.getClientMoney() >= vendingMachineService.findLeastPrice()){
-            String itemPrice = InputController.clientBuyingItem(vendingMachineService.getClientMoney(), vendingMachineService);
-            vendingMachineService.pickOneItem(itemPrice);
-        }
-        OutputView.printCalculateCharge(vendingMachine.getCoinMap(),
-            vendingMachineService.getClientMoney());
+        VendingMachineService vmService = preService();
+        buyItems(vmService);
+        OutputView.printCalculateCharge(vmService.getVendingMachine().getCoinMap(),
+            vmService.getClientMoney());
     }
 
+    private Map<Integer,Integer> makeCoinMap(int money){
+        Map<Integer, Integer> coinMap = UtilService.makeCoinListWithRandoms(money);
+        OutputView.printCoinMap(coinMap);
+        return coinMap;
+    }
+
+    private VendingMachineService preService(){
+        int money = InputController.machineHavingMoney();
+        Map<Integer, Integer> coinMap = makeCoinMap(money);
+        List<String> items = InputController.machineHavingItems();
+        return makeVendingMachineService(money, coinMap,items);
+    }
+
+    private VendingMachineService makeVendingMachineService(int money,Map<Integer,Integer> coinMap,List<String> items){
+        VendingMachine vendingMachine = new VendingMachine(money, coinMap, items);
+        return new VendingMachineService(vendingMachine,
+            InputController.clientToMachineMoney());
+    }
+
+    private void buyItems(VendingMachineService vmService){
+        while(vmService.getClientMoney() >= vmService.findLeastPrice()){
+            String itemPrice = InputController.clientBuyingItem(vmService.getClientMoney(), vmService);
+            vmService.pickOneItem(itemPrice);
+        }
+    }
 }
