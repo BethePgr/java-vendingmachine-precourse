d1a602385040e1b1ca9f07e958ce0eea93f441d1
jongwonpc <c3392302@naver.com>
2023-05-05 10:12:26 +0900

    refactor: 자판기가 보유한 동전 출력하는 기능 리팩토링 및 잔돈 출력 기능 구현

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -8,10 +8,20 @@ public class OutputView {
 
     public static void printCoinMap(Map<Integer, Integer> coinMap) {
         System.out.println("자판기가 보유한 동전");
+        printSortedMap(coinMap);
+
+    }
+
+    public static void printCalculateCharge(Map<Integer, Integer> coinMap){
+        System.out.println("잔돈");
+        printSortedMap(coinMap);
+    }
+
+    private static void printSortedMap(Map<Integer, Integer> coinMap){
         coinMap.entrySet().stream()
             .sorted(Collections.reverseOrder(Map.Entry.comparingByKey()))
             .forEach(entry -> System.out.println(entry.getKey()+"원 - "+ entry.getValue()+"개"));
-
     }
+
 
 }
